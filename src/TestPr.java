9940b4efab9365750f83a8e1d01452c9ffbdb77a
unknown <admin228@C1010-teacher.i-net.ii.npu.edu.ua>
2016-01-25 10:40:23 +0200

    Initial commit

---
+++ src/TestPr.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Created by admin228 on 25.01.2016.
+ */
+public class TestPr {
+
+    final static int N = 5;
+    private static ArrayList<Integer> alist = new ArrayList<Integer>();
+
+    public static void main(String args[]) {
+        for (int i = 0; i < N; i++) {
+            alist.add(i);
+        }
+        for (Integer x: alist)
+            System.out.println(x);
+
+        System.out.println("BOOM!!!");
+    }
+
+}
